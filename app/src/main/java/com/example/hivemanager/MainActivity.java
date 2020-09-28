c9b09bbc7b2082bfcd91c17738d1d0f35adc497e
Allison Winter-Reed <38668004+AllisonWinter-Reed@users.noreply.github.com>
2020-09-28 14:49:30 -0500

    Bottom Navigation Setup File Restructure/Refractor

--- app/src/main/java/com/example/hivemanager/MainActivity.java
+++ app/src/main/java/com/example/hivemanager/MainActivity.java
@@ -15,12 +15,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //TODO check for user authentication
+
+
+
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_profile, R.id.navigation_managehives, R.id.navigation_managehives)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
--- app/src/main/java/com/example/hivemanager/ui/hivestatus/HiveStatusFragment.java
+++ app/src/main/java/com/example/hivemanager/ui/hivestatus/HiveStatusFragment.java
@@ -1,23 +1,20 @@
-package com.example.hivemanager.ui.home;
+package com.example.hivemanager.ui.hivestatus;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.hivemanager.R;
 
-public class HomeFragment extends Fragment {
+public class HiveStatusFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_hivestatus, container, false);
     }
 }
