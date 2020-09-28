2f5102181524bfae7d493d5a65587a347f0c3eb5
Milan-Tomic <mtomic@wisc.edu>
2020-09-28 18:57:13 -0500

    Test commit (filled Apiary accessor and mutator).

--- app/src/main/java/Apiary.java
+++ app/src/main/java/Apiary.java
@@ -13,11 +13,19 @@ public class Apiary {
     }
 
 
+    /**
+     * @param hive the Hive to add to this Apiary.
+     */
     public void addHive(Hive hive) {
+        hives.add(hive);
 
     }
 
+    /**
+     * @param hive the Hive to remove from this Apiary.
+     */
     public void removeHive(Hive hive) {
+        hives.remove(hive);
 
     }
 }
