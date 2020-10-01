b63db63c1b72bfd0a4f4ef32d7247a0db532a533
jwpolk2 <jwpolk2@wisc.edu>
2020-10-01 14:56:31 -0500

    Put apiary, hive, and profile into com.example.hivemanager package

--- app/src/main/java/com/example/hivemanager/MainActivity.java
+++ app/src/main/java/com/example/hivemanager/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.text.Editable;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,7 +23,7 @@ import com.example.hivemanager.ui.profile.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static Editable userName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
