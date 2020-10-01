f5508d8a0381e11e6c6fc68bec5dcb02caf16e2e
Jessica <jmiyake@wisc.edu>
2020-10-01 09:03:31 -0500

    updated login UI

--- app/src/main/java/com/example/hivemanager/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/hivemanager/ui/home/HomeFragment.java
@@ -9,10 +9,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.hivemanager.R;
+import com.example.hivemanager.ui.hivestatus.HiveStatusFragment;
+import com.example.hivemanager.ui.managehives.ManageHivesFragment;
+import com.example.hivemanager.ui.profile.ProfileFragment;
 
 public class HomeFragment extends Fragment {
 
@@ -20,4 +24,28 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+//    public void openHiveStatus(View view) {
+//        Fragment fragment = new HiveStatusFragment();
+//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//        transaction.replace(((R.id.frag_container_home)), fragment);
+//        transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
+//        transaction.commit();
+//    }
+//
+//    public void openManageHives(View view) {
+//        Fragment fragment = new ManageHivesFragment();
+//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//        transaction.replace(((R.id.frag_container_home)), fragment);
+//        transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
+//        transaction.commit();
+//    }
+//
+//    public void openProfile(View view) {
+//        Fragment fragment = new ProfileFragment();
+//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//        transaction.replace(((R.id.frag_container_home)), fragment);
+//        transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
+//        transaction.commit();
+//    }
 }
