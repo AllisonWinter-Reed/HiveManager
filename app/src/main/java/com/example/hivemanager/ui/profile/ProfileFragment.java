0abd580bc73e48395cb6e3ba03edcdd1595a3c73
Sam <sgronseth@wisc.edu>
2020-10-01 20:41:25 -0500

    attemping to add edit profile functionality

--- app/src/main/java/com/example/hivemanager/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/hivemanager/ui/profile/ProfileFragment.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.hivemanager.R;
@@ -15,7 +19,18 @@ public class ProfileFragment extends Fragment {
 
      public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+         Button editProfileBtn = (Button) view.findViewById(R.id.button_editProfile);
+         editProfileBtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 EditProfileFragment editProfileFragment = new EditProfileFragment();
+                 FragmentManager manager = getChildFragmentManager();
+                 manager.beginTransaction().replace(R.id.frag_container_profile, editProfileFragment,
+                         editProfileFragment.getTag()).commit();
+             }
+         });
+         return view;
     }
     public void editProfile(View view) {
     }
