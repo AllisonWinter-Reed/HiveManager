2fa49f133dfc1947f6d3fb0810bffcefdb9c4df7
Sam <sgronseth@wisc.edu>
2020-10-01 23:46:41 -0500

    WIP adding upload profile pic

--- app/src/main/java/com/example/hivemanager/ui/profile/EditProfileFragment.java
+++ app/src/main/java/com/example/hivemanager/ui/profile/EditProfileFragment.java
@@ -1,14 +1,27 @@
 package com.example.hivemanager.ui.profile;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.example.hivemanager.R;
+import com.example.hivemanager.RegisterActivity;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class EditProfileFragment extends Fragment {
+    public static final int GET_FROM_GALLERY = 3;
 
     public EditProfileFragment() {
 
@@ -16,6 +29,39 @@ public class EditProfileFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_editprofile, container, false);
+        View view = inflater.inflate(R.layout.fragment_editprofile, container, false);
+//        Button uploadImage = (Button) view.findViewById(R.id.addImage);
+//        uploadImage.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//            }
+//        });
+
+        return view;
     }
+
+//    public void registerProfilePic(View view){
+//        startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY);
+//
+//    }
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//
+//        //Detects request codes
+//        if(requestCode==GET_FROM_GALLERY && resultCode == RegisterActivity.RESULT_OK) {
+//            Uri selectedImage = data.getData();
+//            Bitmap bitmap = null;
+//            try {
+//                bitmap = MediaStore.Images.Media.getBitmap(getActivity().getApplicationContext().getContentResolver(), selectedImage);
+//            } catch (FileNotFoundException e) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 }
